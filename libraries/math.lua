f6bdeaf615a4cd4026ed1fce92c1d24a5100f8f7
Neural <110314959+Neural0@users.noreply.github.com>
2024-10-19 06:02:33 -0400

    Update math.lua

--- libraries/math.lua
+++ libraries/math.lua
@@ -1,1 +1,8 @@
+local exmath = {}
 
+function exmath.sum(num1,num2) return num1 + num2 end
+function exmath.dif(num1,num2) return num1 - num2 end
+function exmath.quo(num1,num2) return num1 / num2 end
+function exmath.pro(num1,num2) return num1 * num2 end
+
+return exmath
