ea4bf10c465bd1da347a7ecf53ae31f1e111504d
neural <110314959+neuralls@users.noreply.github.com>
2024-10-20 16:05:59 -0400

    Delete libraries/exmath.lua

--- libraries/exmath.lua
+++
@@ -1,8 +0,0 @@
-local exmath = {}
-
-function exmath.sum(num1,num2) return num1 + num2 end
-function exmath.dif(num1,num2) return num1 - num2 end
-function exmath.quo(num1,num2) return num1 / num2 end
-function exmath.pro(num1,num2) return num1 * num2 end
-
-return exmath
